95fd1ccb9830d1c449fddc34d98eaa9d6efb7080
Vini Silveira <viniciusps_ph@hotmail.com>
2025-03-27 18:26:15 -0300

    Primeiro Commit

---
+++ src/main/java/com/example/algamoney/api/model/Categoria.java
@@ -0,0 +1,37 @@
+package com.example.algamoney.api.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "categoria")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nome;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+
+}
